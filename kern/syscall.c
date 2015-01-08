c364d031392ee2b3944c394b9c10d0a43f9ba658
Dmitry Ovchinnikov <ovchinnikov.d94@gmail.com>
2015-01-08 10:38:04 +0300

    rrrrr

--- kern/syscall.c
+++ kern/syscall.c
@@ -137,7 +137,16 @@ sys_env_set_trapframe(envid_t envid, struct Trapframe *tf)
 	// LAB 11: Your code here.
 	// Remember to check whether the user has supplied us with a good
 	// address!
-	panic("sys_env_set_trapframe not implemented");
+	//panic("sys_env_set_trapframe not implemented");
+	struct Env* env;
+	if (envid2env(envid, &env, 1)){
+		panic("sys_env_set_trapframe: Invalid envid");
+		return -E_BAD_ENV;
+	}
+	env->env_tf = *tf;
+	env->env_tf.tf_eflags |= FL_IF;
+	env->env_tf.tf_cs |= 3;
+	return 0;
 }
 
 // Set the page fault upcall for 'envid' by modifying the corresponding struct
@@ -454,6 +463,8 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
 			return sys_ipc_try_send((envid_t)a1,(uint32_t)a2,(void *)a3,(unsigned)a4);
 		case SYS_ipc_recv:
 			return sys_ipc_recv((void *)a1);
+		case SYS_env_set_trapframe:
+			return sys_env_set_trapframe((envid_t )a1, (struct Trapframe *)a2);
 		default:
 			cprintf("%d\n", syscallno);
 			panic("syscall not implemented");
